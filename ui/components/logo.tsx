da3be8e25d5f20b49607d3ceaa561e9f9a0ef3a9
eLeontev <joonior199@gmail.com>
2023-01-02 22:44:00 +0100

    #10 improve welcome screen and UI and add link to logo

--- ui/components/logo.tsx
+++ ui/components/logo.tsx
@@ -1,9 +1,16 @@
 import { Group, Title } from '@mantine/core';
 import Image from 'next/image';
 
+import { InlineLink } from './link.component';
+
+import { rootPageUrl } from '../../constants/urls';
+
 const logoSize = 36;
+
 const logoSrc = '/hearthstone-logo.svg';
 const logoAlt = 'HS BG';
+
+const logoLabel = 'HS BG Competition';
 
 export const Logo = () => (
     <Group>
@@ -11,8 +18,10 @@ export const Logo = () => (
             width={logoSize}
             height={logoSize}
             src={logoSrc}
-            alt={logoSrc}
+            alt={logoAlt}
         ></Image>
-        <Title order={4}>HS BG Competition</Title>
+        <Title order={4}>
+            <InlineLink href={rootPageUrl} label={logoLabel}></InlineLink>
+        </Title>
     </Group>
 );
--- ui/components/welcome.tsx
+++ ui/components/welcome.tsx
@@ -4,20 +4,26 @@ import { LoggedInInfo } from './logged-in-info';
 import { LoginInfo } from './login-info';
 
 import { getLogin } from '../../utils.ts/storage.utils';
-import {Container, Space, Title} from "@mantine/core";
+import { Center, Container, Space, Title } from '@mantine/core';
 
 export const WelcomeScreen = () => {
     const isLoggedIn = getLogin();
 
     return (
         <Container>
-            <Title order={1} mx="">Welcome to Hearthstone Battlegrounds Competition</Title>
-          <Space h="xl"></Space>
-            {isLoggedIn ? (
-                <LoggedInInfo></LoggedInInfo>
-            ) : (
-                <LoginInfo></LoginInfo>
-            )}
+            <Center>
+                <Title order={1}>
+                    Welcome to Hearthstone Battlegrounds Competition
+                </Title>
+            </Center>
+            <Space h="xl"></Space>
+            <Center>
+                {isLoggedIn ? (
+                    <LoggedInInfo></LoggedInInfo>
+                ) : (
+                    <LoginInfo></LoginInfo>
+                )}
+            </Center>
         </Container>
     );
 };
