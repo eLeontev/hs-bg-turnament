3c07d2d7d46f236b5f19f18f92ef5f7be6358983
eLeontev <joonior199@gmail.com>
2023-01-08 17:43:00 +0100

    #21 improve room naming

--- utils.ts/socket.utils.ts
+++ utils.ts/socket.utils.ts
@@ -6,7 +6,7 @@ import { socketRooms } from '../enums/socket.enums';
 import { GameId } from '../models/common.models';
 
 const getOnlineGamePrefix = (isPlayGame?: boolean) =>
-    isPlayGame ? 'online' : 'pending';
+    isPlayGame ? 'play' : 'pending';
 
 export const getSocket = (res: NextApiResponse): Server =>
     (res?.socket as any)?.server?.io as Server;
