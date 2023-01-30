c6696e9de8e5797328d5ef5201d768a871fc53fd
eLeontev <joonior199@gmail.com>
2023-01-30 22:33:44 +0100

    #38 add combat/recruit phase logic/toggle functionality

--- features/play-game/components/recruit/play-game.recruit.desk.tsx
+++ features/play-game/components/recruit/play-game.recruit.desk.tsx
@@ -1,7 +1,14 @@
+import { useInitialRecruitPhaseData } from '../../hooks/play-game.hooks';
 import { usePlayGameStore } from '../store/play-game.store';
 
-export const RecruitDesk = () => {
+const Test = () => {
     const data = usePlayGameStore();
     console.log(data);
     return <>recruit desk</>;
+};
+
+export const RecruitDesk = () => {
+    useInitialRecruitPhaseData();
+
+    return <Test></Test>;
 };
