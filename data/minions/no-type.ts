13d9a316ce4b52fecbc3786bc7a70e38a073dc70
eLeontev <joonior199@gmail.com>
2023-02-04 17:56:54 +0100

    #41 add all/noType minion types

---
+++ data/minions/no-type.ts
@@ -0,0 +1,15 @@
+import { minionIds } from '@prisma/client';
+
+import { Minion } from '../../features/play-game/models/play-game.minion.models';
+
+export const noType1StartTavern = new Map<minionIds, Minion>([]);
+
+export const noType2StartTavern = new Map<minionIds, Minion>([]);
+
+export const noType3StartTavern = new Map<minionIds, Minion>([]);
+
+export const noType4StartTavern = new Map<minionIds, Minion>([]);
+
+export const noType5StartTavern = new Map<minionIds, Minion>([]);
+
+export const noType6StartTavern = new Map<minionIds, Minion>([]);
